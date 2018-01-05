0ee8668f43c58ab0302d9bc27466172943fb5cb2
Rokan Faruqui <rufaruqui@gmail.com>
2018-01-05 14:33:46 +0600

    mail setting - changed

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -5,19 +5,19 @@ class ReportMailer < ApplicationMailer
   def daily_email_update(options={}) 
     puts "Updating daily reports"
     
-    attachments['simple.xlsx'] = File.read('simple.xlsx')
-    mail(to: 'rufaruqui@gmail.com', subject: 'SAPL Report with attacment')
+    # attachments['simple.xlsx'] = File.read('simple.xlsx')
+    # mail(to: 'rufaruqui@gmail.com', subject: 'SAPL Report with attacment')
 
    
-    # mail = Mail.new do 
-    #   from     'sapl.mailer@gmail.com'
-    #   to       'rufaruqui@gmail.com;shimulcse@gmail.com'
-    #   subject  'Stock Report'
-    #   body     'Sample body'
-    #   add_file :filename => options[:filename], :content => File.read(options[:filename])
-    # end
+    mail = Mail.new do 
+      from     'sapl.mailer@gmail.com'
+      to       'rufaruqui@gmail.com;shimulcse@gmail.com'
+      subject  'Stock Report'
+      body     'Sample body'
+      add_file :filename => options[:filename], :content => File.read(options[:filename])
+    end
    
-    # mail.deliver
+    mail.deliver!
   end
 
 end
