103c2ac8d7a2984168f7ca1390588e4b99a3896f
Rokan Faruqui <rufaruqui@gmail.com>
2018-01-25 19:04:55 +0600

    code dir again

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, "ssh://git@203.202.249.101:7999/sod/sodmsmailer.git"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
- set :deploy_to, "/var/www/sodmsmailer/"
+ set :deploy_to, "/var/www/sodmsmailer/code"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
