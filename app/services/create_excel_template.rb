0ef217e817254acc17cf59c047fee65d2b8a1dfb
Rokan Faruqui <rufaruqui@gmail.com>
2018-12-03 13:29:10 +0600

    duplicate container data merged

--- app/services/create_excel_template.rb
+++ app/services/create_excel_template.rb
@@ -8,7 +8,7 @@ class CreateExcelTemplate
     end
    
     def self.prepare_workbook(sheet, data, style_info, hidden=true) 
-
+            damage_details = [:damageArea, :damagePart, :damageDescription]
            if data.blank? or !data.first.include?:containerNumber or data.first[:id] == 0
              sheet.add_row [""], style: style_info[:heading], height: 16   
            else
@@ -18,20 +18,26 @@ class CreateExcelTemplate
            sheet.add_row ["SL #"].push(data.first.keys.map(&:to_s).map(&:titleize)).flatten, style: style_info[:heading]  
           if data.first[:id] != 0
             sl = 1
-             data.each do |info|
-               sheet.add_row    [sl].push(info.values).flatten
+            data.each_with_index do |info, index|
+               a = info.keys 
+              if index > 0 and info.include?:containerNumber and data[index][:containerNumber] == data[index-1][:containerNumber]
+                 sheet.add_row  a.map{|item| (item.in? damage_details ) ? info[item] : nil}
+              else
+                sheet.add_row    [sl].push(info.values).flatten
                sl +=1
+              end
+
             end    
           else
              sheet.add_row [""], style: style_info[:heading], height: 16   
           end
            format_workbook sheet, data.first.keys, style_info
-            sheet.column_info.second.hidden = hidden 
+           sheet.column_info.second.hidden = hidden 
     end
 
     def self.format_workbook(sheet, data, style_info) 
       data.each_with_index do |item, index|  
-        sheet.col_style index+1, style_info[:date_format], :row_offset => 6 if item.to_s.downcase.include?  "date" 
+        sheet.col_style index+1, style_info[:date_format], :row_offset => 5 if item.to_s.downcase.include?  "date" 
       end
         sheet.column_info.first.width = 5
         
