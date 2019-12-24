a4f4ae841797d400fe07a7f642cab4dc82d0cc19
Rokan Faruqui <rufaruqui@gmail.com>
2019-12-24 09:55:07 +0600

    isatl mail credential changed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,24 +89,24 @@ Rails.application.configure do
   # config.action_mailer.default_options = {from: 'reporting@isatlbd.com'}
   
 
-  config.action_mailer.smtp_settings = {
-      :address => "smtp.office365.com",
-      :port => 587,
-      :domain => 'saplbd.com',
-      :user_name => "reporting@saplbd.com",
-      :password => "it@sapl$9904",
-      :authentication => :login,
-      :enable_starttls_auto => true
-  }
   # config.action_mailer.smtp_settings = {
   #     :address => "smtp.office365.com",
   #     :port => 587,
-  #     :domain => 'isatlbd.com',
-  #     :user_name => "reporting@isatlbd.com",
-  #     :password => "25846@#isatl",
+  #     :domain => 'saplbd.com',
+  #     :user_name => "reporting@saplbd.com",
+  #     :password => "it@sapl$9904",
   #     :authentication => :login,
   #     :enable_starttls_auto => true
   # }
+  config.action_mailer.smtp_settings = {
+      :address => "smtp.office365.com",
+      :port => 587,
+      :domain => 'isatlbd.com',
+      :user_name => "reporting@isatlbd.com",
+      :password => "25846@#isatl",
+      :authentication => :login,
+      :enable_starttls_auto => true
+  }
   # config.action_mailer.smtp_settings = {
   #     :address => "smtp.gmail.com",
   #     :port => 587,
